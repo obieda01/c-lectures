25a314ee15174b50891d538311e90d98429ea730
John Fulton <john@TechElevator.com>
2017-01-19 14:42:20 -0500

    m1-w1d5-command-line

--- m1-w1d4-loops-arrays-lecture/Arrays/Program.cs
+++ m1-w1d4-loops-arrays-lecture/Arrays/Program.cs
@@ -10,27 +10,40 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-           
+
             //1. Create an array of integers that can hold quiz scores 
-            
+
+            int[] quizScores = new int[100];
+
+            int arrayLength = quizScores.Length;
+
             //2. Create an array of strings that can hold the names of instructors at TE
-            
-            //3. Create an array of characters that hold "Tech Elevator"        
-                    
+
+            string[] instructorNames = { "John", "Joe", "Casey" };
+
+            //3. Create an array of characters that hold "Tech Elevator"    
+
+            char[] teChar = { 'T', 'e', 'c', 'h', ' ' };
+
             //4. Print out the 0th item in each array
+            Console.WriteLine(quizScores[0]);
 
             //5. Print out the 3rd item in each array
+            Console.WriteLine(quizScores[2]);
 
             //6. Get the length of each array
+            int x = quizScores.Length;
 
             //7. Get the last index for each array
-            
+            int y = quizScores.Length - 1;
+
             //6. Update the last item in each array
+            quizScores[quizScores.Length - 1] = 7;
 
             //7. Loop over an array's contents and print each item out to the screen
 
             //8. Loop over an array and print every other item out to the screen
-            
+
         }
     }
 }
--- m1-w1d4-loops-arrays-lecture/ForLoops/Program.cs
+++ m1-w1d4-loops-arrays-lecture/ForLoops/Program.cs
@@ -11,15 +11,42 @@ namespace ForLoops
         static void Main(string[] args)
         {
             //1. How would we print out "Hello World" 10 times using a for loop?
+
+            for (int x = 0; x < 10; x++) 
+            {
+                Console.WriteLine("Hello World");
+            }
             
 
             //2. How would we print out the numbers 1 through 50 using a for loop?
+
+            for(int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+            }
                         
 
             //3. How would we only print out odd numbers 1 through 50 using a for loop?
-            
+
+            for (int i = 1; i <=50; i = i +2)
+            {
+                Console.Write(" " + i);
+            }
+
+            Console.WriteLine();
+
 
             //4. How would we find the sum of even numbers 1 through 50 using a for loop?
+
+            int sumOfEvens = 0;
+
+            for(int i = 2; i <=50; i +=2)
+            {
+                sumOfEvens += i;
+            }
+
+            Console.Write(sumOfEvens);
+
                         
         }
     }
