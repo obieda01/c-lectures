a3f5d43fdaf80dac30836a7008ee79e5854cedd6
John Fulton <john@TechElevator.com>
2017-02-02 11:36:06 -0500

    m1-w3d5-tdd-lecture

---
+++ m1-w3d5-tdd-lecture/TDDLectureTests/KataPrimeFactorsTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDLecture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDLecture.Tests
+{
+    [TestClass()]
+    public class KataPrimeFactorsTests
+    {
+        
+    }
+}
