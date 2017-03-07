0d48aa352e26a42222d7bda672fd5d414460496e
John Fulton <john@TechElevator.com>
2017-03-07 13:04:10 -0500

    m3-w2d2A-voter-information-lecture

---
+++ m3-w2d2A-voter-information-lecture/VoterInformation/DAO/VoterDAO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using VoterInformation.Models;
+
+namespace VoterInformation.DAO
+{
+    public class VoterDAO
+    {
+        string connectionString = @"Data Source=DESKTOP-QSBFLBV;Initial Catalog=VoterData;Integrated Security=True";
+        string SQL_VotersforStreet = "SELECT FIRSTNAME, LASTNAME, RES_HOUSE, RES_STREET FROM Voter WHERE RES_STREET LIKE CONCAT('%',@streetName,'%') ORDER BY LASTNAME, FIRSTNAME;";
+
+
+        public List<VoterModel> GetVotersByStreet(string streetName)
+        {
+            List<VoterModel> result = new List<VoterModel>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_VotersforStreet, conn);
+
+                    cmd.Parameters.AddWithValue("@streetName", streetName);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        VoterModel v  = new VoterModel();
+                        v.FirstName = Convert.ToString(reader["FIRSTNAME"]);
+                        v.LastName  = Convert.ToString(reader["LASTNAME"]);
+                        v.StreetNumber = Convert.ToString(reader["RES_HOUSE"]);
+                        v.StreetName = Convert.ToString(reader["RES_STREET"]);
+                        result.Add(v);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                //Log and throw the exception
+                throw;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ m3-w2d2A-voter-information-lecture/VoterInformation/Models/VoterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VoterInformation.Models
+{
+    public class VoterModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetNumber { get; set; }
+        public string StreetName { get; set; }
+
+    }
+}
