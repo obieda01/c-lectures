2595ad34f438363378a0a7dd891a59076d757b0f
Obieda01 <obieda01@email.franklin.edu>
2017-03-22 09:21:03 -0400

    preparing to pull from upstream

--- m4-w1d2-intro-jquery-and-dom-lecture/script.js
+++ m4-w1d2-intro-jquery-and-dom-lecture/script.js
@@ -1,16 +1,16 @@
-﻿$(document).ready(function () {
+﻿$(document).ready(function() {
 
     //1. Getting an HTML element by id
-    var box1 = $("#box1");    
+    var box1 = $("#box1");
     console.log(box1);
-        
+
     //2. Getting an HTML element by class name
     var redBoxes = $(".red");
     console.log(redBoxes);
 
     //3. Getting HTML elements by tag name
     var divBoxes = $("div");
-    console.log(divBoxes);   
+    console.log(divBoxes);
 
     //4. Filtering the div boxes to include only odd-numbered boxes
     var oddNumberedBoxes = $("div").filter(":odd");
@@ -22,7 +22,7 @@
     //OR
     var specialItems2 = $("li.special");
     console.log(specialItems2);
-      
+
     //6. Getting the last box
     var lastH2 = $("div.box").last();
     console.log(lastH2);
@@ -45,9 +45,9 @@
 
     //11. Setting the value of the text box to Tech Elevator
     $("#nameField").val("Tech Elevator");
-    
+
     //12. Getting the value of the first <p> tag in the green box
-    var greenBoxPTag = $(".green").children("p")    
+    var greenBoxPTag = $(".green").children("p")
     console.log("Before " + greenBoxPTag.html());
 
     //13. Changing the html
@@ -67,13 +67,13 @@
     //var otherBoxes = $(".box").not(".grey");        
     //blackBox.removeClass("grey").addClass("red");
     //otherBoxes.removeClass().addClass("box").addClass("black");
-    
+
     //18. Adding a new list item
     $("ul").append("<li>Item 6</li>")
 
     //19. Moving the even numbered items from the yellow box to the green box
     var list = $("li").filter(":even").detach();
     $(".green").children().filter("ol").append(list);
-    
+
 
 });
