853e8d311419211f9ee2ce995475efe20ffe5352
Obieda01 <obieda01@email.franklin.edu>
2017-03-22 11:24:52 -0400

    enhance m4 w1 d2 events lecture

--- m4-w1d3-events-lecture/functions/script.js
+++ m4-w1d3-events-lecture/functions/script.js
@@ -5,9 +5,9 @@ Function Expressions and Anonymous Functions
 //We've seen function declarations that allow us to write reusable code like this: 
 
 function speak(numberOfTimes) {
-  for(var i = 0; i < numberOfTimes; i++) {
-      console.log("WOOF");   
-  }
+    for (var i = 0; i < numberOfTimes; i++) {
+        console.log("WOOF");
+    }
 }
 
 speak(5);
@@ -18,9 +18,9 @@ to recursively call it). */
 
 // Note that we are assigning an anonymous function to a variable named `fly`.
 var fly = function(num) {
-    for(var i = 0; i < num; i++) {
-        console.log("I'm flying!!");   
-    }   
+    for (var i = 0; i < num; i++) {
+        console.log("I'm flying!!");
+    }
 };
 
 fly(3); // here we use the `fly` variable to invoke the function.
@@ -35,8 +35,8 @@ thinkHappyThoughts(20);
 
 function filter(things, matcher) {
     var results = [];
-    for(var i = 0; i < things.length; i++) {
-        if(matcher(things[i])) {
+    for (var i = 0; i < things.length; i++) {
+        if (matcher(things[i])) {
             results.push(things[i]);
         }
     }
@@ -51,9 +51,8 @@ function oddNumbers(num) {
     return num % 2 != 0;
 }
 
-var input = [1,2,3,4,5,6,7,8,9];
+var input = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 console.log(filter(input, evenNumbers));
 console.log(filter(input, oddNumbers));
-
-
+console.log(filter(input, evenNumbers));
